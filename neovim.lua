af255d05c908447358a542dbdfe814bbedfeb469
MatheusAssisM <matheusassismelo@gmail.com>
2026-01-01 17:51:10 -0300

    Initial commit: Kingdom Come Deliverance theme

---
+++ neovim.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"sainnhe/gruvbox-material",
+		name = "gruvbox-material",
+		priority = 1000,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "gruvbox-material",
+		},
+	},
+}
